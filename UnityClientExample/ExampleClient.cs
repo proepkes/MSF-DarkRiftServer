f98f76286d74ad2d876e9ff463c310eb5f17d7cf
Phil Börner <pb156875@fh-muenster.de>
2018-03-18 15:48:07 +0100

    Example-Client

---
+++ UnityClientExample/ExampleClient.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using DarkRift;
+using DarkRift.Client;
+using UnityEngine;
+using Utils;
+using Utils.Extensions;
+using Utils.Messages.Requests;
+using Utils.Messages.Response;
+using Message = DarkRift.Message;
+using MessageReceivedEventArgs = DarkRift.Client.MessageReceivedEventArgs;
+using Security = Utils.Security;
+
+namespace UnityClientExample
+{
+    public class ExampleClient : MonoBehaviour
+    {
+        //BEFORE YOU USE THIS CLASS IN YOUR UNITY-PROJECT: Change the type of Client from UnityClientMock to DarkRift's original "UnityClient"
+        public UnityClientMock Client;
+        public bool RequestAESKey;
+
+        private int RsaKeySize = 512;
+        private string _clientAesKey;
+
+        private bool _isLoggingIn, _aesKeyPending;
+        private RSACryptoServiceProvider clientsCsp;
+        private RSAParameters parameters;
+        private string aesKey = String.Empty;
+
+        public event Action LoggedIn;
+        public event Action<ResponseStatus, string> LogInFailed;
+        public event Action Registered;
+        public event Action LoggedOut;
+        public bool IsLoggedIn { get; protected set; }
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+
+        void Start()
+        {
+            Client.MessageReceived += ClientOnMessageReceived;
+            Client.Disconnected += ClientOnDisconnected;
+
+            if (RequestAESKey)
+                StartCoroutine(RequestAesKey());
+        }
+
+        private void ClientOnDisconnected(object sender, DisconnectedEventArgs disconnectedEventArgs)
+        {
+            IsLoggedIn = false;
+        }
+
+        private void ClientOnMessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            using (var message = e.GetMessage())
+            {
+                switch (message.Tag)
+                {
+                    case MessageTags.RequestAesKeyResponse:
+                        HandleRequestAesKeyResult(message);
+                        break;
+                    case MessageTags.LoginSuccessResponse:
+                        HandleLoginSuccess(message);
+                        break;
+                    case MessageTags.LoginFailedResponse:
+                        HandleLoginFailed(message);
+                        break;
+                    case MessageTags.RegisterAccountSuccessResponse:
+                        HandleRegisterAccountSuccess();
+                        break;
+                    case MessageTags.RegisterAccountFailedResponse:
+                        HandleRegisterAccountFailed(message);
+                        break;
+                    case MessageTags.RequestPasswordResetSuccessResponse:
+                        HandlePasswordResetSuccess();
+                        break;
+                    case MessageTags.RequestPasswordResetFailedResponse:
+                        HandlePasswordResetFailed(message);
+                        break;
+                }
+            }
+        }
+
+        #region AUTH
+        private void HandlePasswordResetFailed(Message message)
+        {
+            var msg = message.Deserialize<RequestFailedMessage>();
+            if (msg != null)
+            {
+                //Handle
+            }
+        }
+
+        private void HandlePasswordResetSuccess()
+        {
+            //Handle
+        }
+
+        private void HandleRegisterAccountFailed(Message message)
+        {
+            var msg = message.Deserialize<RequestFailedMessage>();
+            if (msg != null)
+            {
+                //Handle
+            }
+        }
+
+        private void HandleRegisterAccountSuccess()
+        {
+            Registered?.Invoke();
+
+            _isLoggingIn = false;
+        }
+
+        private void HandleLoginFailed(Message message)
+        {
+            var msg = message.Deserialize<RequestFailedMessage>();
+            if (msg != null)
+            {
+                LogInFailed?.Invoke(msg.Status, msg.Reason);
+            }
+            _isLoggingIn = false;
+        }
+
+        private void HandleLoginSuccess(Message message)
+        {
+            var msg = message.Deserialize<LoginSuccessMessage>();
+            if (msg != null)
+            {
+                if (msg.Status == ResponseStatus.Success)
+                {
+                    IsLoggedIn = true;
+                    LoggedIn?.Invoke();
+                }
+                _isLoggingIn = false;
+            }
+        }
+
+        private void HandleRequestAesKeyResult(Message message)
+        {
+            using (DarkRiftReader reader = message.GetReader())
+            {
+                var response = reader.ReadBytes();
+
+                if (response == null)
+                {
+                    Console.WriteLine("Failed to receive aes key");
+                    return;
+                }
+
+                var decrypted = clientsCsp.Decrypt(response, false);
+                aesKey = Encoding.Unicode.GetString(decrypted);
+            }
+        }
+
+        private IEnumerator RequestAesKey()
+        {
+            while (!Client.Connected)
+            {
+                yield return new WaitForSeconds(.2f);
+            }
+
+            if (Client.Connected)
+            {
+                //Request AES key
+                clientsCsp = new RSACryptoServiceProvider(RsaKeySize);
+                parameters = clientsCsp.ExportParameters(false);
+
+                // Serialize public key
+                var sw = new StringWriter();
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+                xs.Serialize(sw, parameters);
+
+                using (var writer = DarkRiftWriter.Create(Encoding.Unicode))
+                {
+                    writer.Write(sw.ToString());
+                    Client.SendMessage(Message.Create(MessageTags.RequestAesKey, writer), SendMode.Reliable);
+                }
+            }
+        }
+
+        public void LogIn(string email, string password)
+        {
+            if (!Client.Connected)
+            {
+                return;
+            }
+
+            if (_isLoggingIn)
+            {
+                return;
+            }
+
+            _isLoggingIn = true;
+
+            var data = new Dictionary<string, string>
+            {
+                {"email", email},
+                {"password", password}
+            };
+
+            var encryptedData = Security.EncryptAES(data.ToBytes(), aesKey);
+            using (var writer = DarkRiftWriter.Create())
+            {
+                writer.Write(encryptedData);
+                Client.SendMessage(Message.Create(MessageTags.LogIn, writer), SendMode.Reliable);
+            }
+        }
+
+        public void LogOut()
+        {
+            if (!Client.Connected)
+            {
+                return;
+            }
+
+            Client.Disconnect();
+
+            LoggedOut?.Invoke();
+        }
+
+        public void Register(string email, string password)
+        {
+            var data = new Dictionary<string, string>
+            {
+                {"email", email},
+                {"password", password},
+            };
+
+            var encryptedData = Security.EncryptAES(data.ToBytes(), aesKey);
+            using (var writer = DarkRiftWriter.Create())
+            {
+                writer.Write(encryptedData);
+                Client.SendMessage(Message.Create(MessageTags.RegisterAccount, writer), SendMode.Reliable);
+            }
+        }
+
+        public void RequestPasswordReset(string eMail, string code, string newPassword)
+        {
+            Client.SendMessage(
+                Message.Create(MessageTags.RequestPasswordReset,
+                    new RequestResetPasswordMessage { EMail = eMail, Code = code, NewPassword = newPassword }),
+                SendMode.Reliable);
+        }
+
+        public void RequestPasswordResetCode(string eMail)
+        {
+            //Fire & Forget
+            Client.SendMessage(
+                Message.Create(MessageTags.RequestPasswordResetCode, new RequestWithEmailMessage { EMail = eMail }),
+                SendMode.Reliable);
+        }
+
+        public void ConfirmEmail(string email, string code)
+        {
+            Client.SendMessage(
+                Message.Create(MessageTags.ConfirmEmail, new RequestEmailConfirmationMessage { EMail = email, Code = code }),
+                SendMode.Reliable);
+        }
+
+        public void RequestNewEmailConfirmationCode(string email)
+        {
+            Client.SendMessage(
+                Message.Create(MessageTags.RequestNewEmailConfirmationCode, new RequestWithEmailMessage { EMail = email }),
+                SendMode.Reliable);
+        }
+
+        #endregion
+
+        #region ROOM
+
+        #endregion
+    }
+}
---
+++ UnityClientExample/UnityClientMock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DarkRift;
+using DarkRift.Client;
+
+namespace UnityClientExample
+{
+    public abstract class UnityClientMock
+    {
+        public event EventHandler<DisconnectedEventArgs> Disconnected;
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+        public bool Connected { get; set; }
+
+        public abstract bool SendMessage(Message create, SendMode reliable);
+        public abstract bool Disconnect();
+    }
+}
