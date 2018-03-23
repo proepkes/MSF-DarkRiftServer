b15b2c2e4067cee97c41745f6a02d7d9169fc1c8
proepkes <pb156875@fh-muenster.de>
2018-03-23 19:51:17 +0100

    Move NetworkTimePlugin to Unity

---
+++ Utils/Packets/TimePacket.cs
@@ -0,0 +1,19 @@
+﻿using DarkRift;
+
+namespace Utils.Packets
+{
+    public class TimePacket : IDarkRiftSerializable
+    {
+        public float Time;
+
+        public void Deserialize(DeserializeEvent e)
+        {
+            Time = e.Reader.ReadSingle();
+        }
+
+        public void Serialize(SerializeEvent e)
+        {
+            e.Writer.Write(Time);
+        }
+    }
+}
